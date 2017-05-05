460596a933b8b3a896e7d8755ec7bd3cd85eb480
Juan Manuel Mougan <juanmougan@gmail.com>
2017-05-04 23:39:15 -0300

    Lesson 49

---
+++ src/containers/book-detail.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+    return (
+      <div>Book Detail!</div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    // will show as props
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail)
